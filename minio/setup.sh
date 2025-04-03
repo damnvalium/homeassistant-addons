a0fb1253a86b74b717ef6c69ffe4156dfd11adc0
Valerio Ferrandino <val.ferrandino@gmail.com>
2025-04-03 04:25:03 +0200

    MinIO: Update

--- minio/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# shellcheck shell=bash
-
-CONFIG_PATH=/data/options.json
-MINIO_ROOT_USER="$(bashio::config 'root_user')"
-MINIO_ROOT_PASSWORD="$(bashio::config 'root_password')"
